3a51d0f9aa4c97a353a228c3ead97dd220d9996a
DaniarOD <daniar.oktavian@gmail.com>
2023-05-24 10:57:25 +0700

    after uas demo 12

--- Minggu 9/Praktikum/Praktikum 1/4d.c
+++ Minggu 9/Praktikum/Praktikum 1/4d.c
@@ -1,28 +1,32 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<time.h>
 
 int faktorial_it(int);
 int faktorial_re(int);
 int faktorial_retail(int, int);
-void menu();
+void waktu(int, int(*)(int));
+void menu(int);
 
 int main()
 {
+    int n;
+
+    printf("Program Mencari faktorial dari suatu bilangan\n");
+    printf("Masukkan bilangan yang akan difaktorialkan : ");
+    scanf("%d", &n);
     while (1)
     {
-        menu();
+        menu(n);
     }
     
     return 0;
 }
 
-void menu()
+void menu(int n)
 {
-    int jwb, n;
+    int jwb;
 
-    printf("Program Mencari faktorial dari suatu bilangan\n");
-    printf("Masukkan bilangan yang akan difaktorialkan : ");
-    scanf("%d", &n);
     printf("\nMENU Faktorial\n");
     printf("1. Iterasi\n");
     printf("2. Rekursi\n");
@@ -36,13 +40,13 @@ void menu()
     switch (jwb)
     {
     case 1:
-        printf("Hasil faktorial dari %d adalah %d\n", n, faktorial_it(n));
+        waktu(n, faktorial_it);
         break;
     case 2:
-        printf("Hasil faktorial dari %d adalah %d", n, faktorial_re(n));
+        waktu(n, faktorial_re);
         break;
     case 3:
-        printf("Hasil faktorial dari %d adalah %d\n", n, faktorial_retail(n, 1));
+        waktu(n, faktorial_retail);     //mumet makkk;
         break;
     case 4:
         exit(0);
@@ -51,6 +55,17 @@ void menu()
         printf("Pilihan Anda Invalid");
         break;
     }
+}
+
+void waktu(int n, int(*faktorial)(int))
+{
+    clock_t start, end;
+    double waktu;
+    start = clock();
+    printf("Hasil faktorial dari %d adalah %d\n", n, faktorial(n));
+    end = clock();
+    waktu = ((double) (end - start)) / CLOCKS_PER_SEC;
+    printf("Waktu komputasi : %f detik\n");
 }
 
 int faktorial_it(int n)
@@ -67,7 +82,7 @@ int faktorial_re(int n)
 {
     if(n == 0)
         return 1;
-    return n * faktorial(n-1);
+    return n * faktorial_re(n-1);
 }
 
 int faktorial_retail(int n, int a)
@@ -75,5 +90,5 @@ int faktorial_retail(int n, int a)
     if(n == 0)
         return a;
     else
-        return faktorial(n-1, n*a);
+        return faktorial_retail(n-1, n*a);
 }
